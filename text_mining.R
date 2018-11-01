93726e2a13d3c85d2a6a1aea67cf89870fd4f0e2
Mihir Bhatia <bhatia35@purdue.edu>
2018-10-31 23:46:47 -0400

    Update text_mining.R

--- text_mining.R
+++ text_mining.R
@@ -3,12 +3,22 @@ library(plyr)
 library(dplR)
 library(stringr)
 library(dplyr)
-setwd("W:/pu.data/Desktop/Scrapper_to_use")
+setwd("C:/Users/heman/OneDrive/Documents/DCMME/WHIN")
+
+Certification_list <- read.csv("Certification list.csv")
+
 
 #INITIAL ANALYSIS OF TEXT--------------------------------------------------
 #extracting json file as list 
-result <- fromJSON(file = "example_1.json")
+result <- fromJSON(file = "json_30www.mw-ind.com.json")
 print(result)
+
+
+# data=c()
+# for(i in 1:length(result))
+# {
+#   data[i] = result[[i]]
+# }
 
 #vector of json file
 #vector <- as.character(result)
@@ -30,12 +40,21 @@ word_count <- words_without_stopwords %>% count(word, sort = TRUE)
 
 #TRYING TO LOCATE ISO CERTIFICATIONS----------------------------------------
 
+words_without_stopwords$word=paste("-",words_without_stopwords$word,"-")
+Certification_list$Certifications=paste("-",Certification_list$Certifications,"-")
+
+for(j in Certification_list$Certifications)
+{
+  print(j)
+  data=subset(x = words_without_stopwords,grepl(j,words_without_stopwords$word,ignore.case = T)==TRUE)
+print(data)
+    }
+
 
 
 #TRYING TO FIND WORDS ASSOCIATED WITH PRODUCTS-------------------------------
 
-
-
+a=as.data.frame(findAssocs(dtm,pos_frequency$word[i], 0.1))
 
 #TRYING TO FIND WORDS ASSOCIATED WITH EQUIPMENT ------------------------------
 
